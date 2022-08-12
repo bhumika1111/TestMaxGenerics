e9ee83b4c03616a6e17ca63352af8657bddb69a2
bhumika1111 <bhoyarpatil@434>
2022-08-12 19:06:14 +0530

    Added Maximum Of Four Main Program File

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Maximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("Output For Four Integer Number Using Compare To Method And Check The Maximum Number.");
+            MaximumFour maximumFour = new MaximumFour();
+            int Value = maximumFour.MaximumInteger(11, 22, 33,88);
+            Console.WriteLine(Value);
+
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("Output For Four Integer Number Using Compare To Method By Using Generics And Check The Maximum Number.");
+            MaximumFourGenerics<int> MaximumFour = new MaximumFourGenerics <int>(11, 22, 33, 88);
+            int value1 = MaximumFour.CheckFourGenerics(11, 22, 33, 88);
+            Console.WriteLine("The Maximum Number Is: " + value1);
+
+           
+
+
+        }
+    }
+}
